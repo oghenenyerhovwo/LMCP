4e35ca182a98e8b5e40f6fc39ec568b7a0f82197
oghenenyerhovwo <93879454+oghenenyerhovwo@users.noreply.github.com>
2022-10-14 01:01:08 +0100

    Checking for server production failure

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema;
 
-
 const userSchema = new Schema({
     fullName: {type: String, required: true},
     email: {type: String, required:true, unique: true},
