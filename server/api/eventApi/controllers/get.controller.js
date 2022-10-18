ca8b9aa206d3dd06eef65c5a724941482df0d0da
oghenenyerhovwo <93879454+oghenenyerhovwo@users.noreply.github.com>
2022-10-18 06:51:02 +0100

    added event model to backend

--- server/api/eventApi/controllers/get.controller.js
+++ server/api/eventApi/controllers/get.controller.js
@@ -14,8 +14,8 @@ export const getEvents = async(req, res) => {
 // get Event
 export const getEventsWithLimit = async(req, res) => {
     try {
-        const Events= await Event.find().sort({date: -1}).limit(Number(req.params.limitNumber)).populate("author")
-        res.send({Events}) 
+        const events= await Event.find().sort({date: -1}).limit(Number(req.params.limitNumber)).populate("author")
+        res.send({events}) 
     } catch (error) {
         console.log(error)
         res.status(404).send({message: "Server error: Could not get all Event"})
@@ -28,8 +28,8 @@ export const getEvent = async(req, res) => {
         const EventId= req.params.id
         const foundEvent= await Event.findById(EventId)
         if(foundEvent){
-            const Event = await Event.findById(EventId).populate("author").populate({path: "comments", populate: {path: "author", model: "User"}})
-            res.send({Event})
+            const event = await Event.findById(EventId).populate("author").populate({path: "comments", populate: {path: "author", model: "User"}})
+            res.send({event})
         } else {
             res.status(404).send({message: "Event not found"})
         }    
