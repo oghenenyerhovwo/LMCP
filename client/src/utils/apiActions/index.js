ff5a6f1448f9bcb010d534851a0740e96dade015
oghenenyerhovwo <93879454+oghenenyerhovwo@users.noreply.github.com>
2022-10-20 06:17:06 +0100

    swicthed to development modee

--- client/src/utils/apiActions/index.js
+++ client/src/utils/apiActions/index.js
@@ -1,11 +1,11 @@
 // offline
-// export const backend_url = "http://localhost:5000/api"
-// export const frontend_url = "http://localhost:3000/"
+export const backend_url = "http://localhost:5000/api"
+export const frontend_url = "http://localhost:3000/"
 
 
 // Online
-export const backend_url = "https://lmcp.herokuapp.com/api"
-export const frontend_url = "https://lmcp.vercel.app/"
+// export const backend_url = "https://lmcp.herokuapp.com/api"
+// export const frontend_url = "https://lmcp.vercel.app/"
 
 export const setError = (err) => {
     return err.response && err.response.data.message ? err.response.data.message : err.message
